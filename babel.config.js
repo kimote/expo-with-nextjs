162da97263656145479da5a5959b1193862708fc
zoko kimote <kimotezoko@gmail.com>
2022-07-29 14:30:21 +0200

    first commit

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,24 @@
-module.exports = function(api) {
-  api.cache(true);
-  return {
-    presets: ['babel-preset-expo'],
-  };
+// @generated: @expo/next-adapter@4.0.10
+// Learn more: https://github.com/expo/expo/blob/master/docs/pages/versions/unversioned/guides/using-nextjs.md#shared-steps
+
+//module.exports = { presets: ['@expo/next-adapter/babel'] };
+
+module.exports = {
+ presets: [
+  'next/babel',
+  [
+   'babel-preset-expo',
+   {
+    web: { useTransformReactJSXExperimental: true },
+   },
+  ],
+ ],
+ plugins: [
+  ['@babel/plugin-proposal-class-properties', { loose: true }], 
+  ['@babel/plugin-proposal-private-methods', { loose: true }],
+  [
+   '@babel/plugin-proposal-private-property-in-object',
+   { loose: true }
+  ],
+ ],
 };
---
+++ pages/_document.js
@@ -0,0 +1,2 @@
+// @generated: @expo/next-adapter@4.0.10
+export { default } from '@expo/next-adapter/document';
